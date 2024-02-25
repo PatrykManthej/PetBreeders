b901d4e6c9884b2cdc1997979c4ef50dfef14916
Patryk <manpat1997@gmail.com>
2024-02-25 11:06:46 +0100

    .net 8 upgrade

--- PetBreeders.Application/DependencyInjection.cs
+++ PetBreeders.Application/DependencyInjection.cs
@@ -8,7 +8,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         return services;
     }
